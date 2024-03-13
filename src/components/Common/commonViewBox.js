934b5a4873100d2e3777f04a1443520a2c2f3c98
dpound@naver.com <dpound@naver.com>
2024-03-13 21:30:37 +0900

    메인페이지 홈 부분 뼈대 구상 완료

--- src/components/Common/commonViewBox.js
+++ src/components/Common/commonViewBox.js
@@ -4,14 +4,22 @@ import { StyleSheet, View } from "react-native";
  * 공통으로 사용하는 컴포넌트 뷰 박스
  * 인자를 받아 자유롭게 사용하자
  */
-const commonBox = ()=>{
+const CommonViewBox = () => {
     return (
-        <View>
+        <View style={styles.viewContainer}>
             
         </View>
     );
-}
+};
+
+export default CommonViewBox;
 
 const styles = StyleSheet.create({
-    
-})
+    viewContainer : {
+        width : 150,
+        height: 150,
+        margin: 15,
+        color: 'black',
+        backgroundColor : 'gray',
+    },
+});
--- src/screens/Home/HomeSection.js
+++ src/screens/Home/HomeSection.js
@@ -1,6 +1,7 @@
 import { StyleSheet,Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeSectionCircle from "./HomeSectionComponents/HomeSectionCircle";
+import CommonViewBox from "../../components/Common/CommonViewBox";
 
 const HomeSection = ()=>{
     return (
@@ -11,21 +12,37 @@ const HomeSection = ()=>{
             <View>
                 <HomeSectionCircle />
             </View>
-            <View>
+            <View style={styles.sectionTitleView}>
                 <Text style={styles.sectionTitleText}>진행중인 라이브</Text>
             </View>
-            <View>
+            <View style={styles.viewBoxList}>
+                <CommonViewBox/>
+                <CommonViewBox/>
+            </View>
+            <View style={styles.sectionTitleView}>
                 <Text style={styles.sectionTitleText}>BEST</Text>
             </View>
-            <View>
+            <View style={styles.viewBoxList}>
+                <CommonViewBox/>
+                <CommonViewBox/>
+            </View>
+            <View style={styles.sectionTitleView}>
                 <Text style={styles.sectionTitleText}>종료 임박한 상품</Text>
             </View>
-            <View>
+            <View style={styles.viewBoxList}>
+                <CommonViewBox/>
+                <CommonViewBox/>
+            </View>
+            <View style={styles.sectionTitleView}>
                 <Text style={styles.sectionTitleText}>예정된 경매</Text>
             </View>
-            <View>
-                <TouchableOpacity style={styles.sectionTitleText}>
-                    <Text style={styles.sectionTitleText}>위시가 없나요?</Text>
+            <View style={styles.viewBoxList}>
+                <CommonViewBox/>
+                <CommonViewBox/>
+            </View>
+            <View style={styles.sectionTitleView}>
+                <TouchableOpacity style={styles.wishButton}>
+                    <Text style={styles.wishButtonText}>위시가 없나요?</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -36,8 +53,14 @@ export default HomeSection;
 
 
 const styles = StyleSheet.create({
+    sectionTitleView : {
+        marginTop : 10,
+        marginBottom : 5,
+        marginLeft : 15,
+    },
     sectionTitleText : {
         color : "black",
+        fontSize : 20,
     },
     sectionText : {
         color : "black",
@@ -48,4 +71,22 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+    // 오른쪽 드래그 박스리스트
+    viewBoxList: {
+        flexDirection : 'row'
+    },
+    wishButton : {
+        width : '90%',
+        height : 50,
+        margin : 10,
+        borderRadius : 10,
+        backgroundColor : 'gray',
+    },
+    wishButtonText : {
+        textAlign : 'center',
+        color : 'black',
+        fontSize : 23,
+        fontWeight : '400',
+        marginTop : 10,
+    }
 });
