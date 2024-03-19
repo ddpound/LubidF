d37b09de4cf3631ebe00d6bce7427d1037eba0fb
dpound@naver.com <dpound@naver.com>
2024-03-19 20:32:21 +0900

    mypage 헤더에 토큰을 담아 전송하고 그 값을 가져오도록 함

--- src/screens/MyPage.js
+++ src/screens/MyPage.js
@@ -5,24 +5,36 @@ import { StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native";
 import { useEffect } from "react";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const logout = (logOut) =>{
     console.log('logout');
     logOut();
 }
 
-const MyPage = () => {
-    let jwt;
-    useEffect(()=>{
-        // 로그인되어있는 유저의 개인정보를 가져와야함
-        // 단 바로가져오지말아야하 하지만 일단 바로 띄어주기
+const getLoginData = async ()=>{
+    let jwtValue = await AsyncStorage.getItem("LubidJwt");
+    // 로그인되어있는 유저의 개인정보를 가져와야함
+    // 단 바로가져오지말아야하 하지만 일단 바로 띄어주기
+    console.log('try get user info');
+    await axios.get('http://116.32.208.215:7777/lubid-user/user/find-user/1',{
+        withCredentials: true,
+        headers: {
+            AUTHORIZATION : "Bearer " + jwtValue
+        }
+    }).then((res)=>{
+        console.log(res.data);
+    }).catch((error)=>{
+        console.log(error);
+    });
+}
 
-        axios.get('',{},{
-            headers: {
-                "AUTHORIZATION" : "Bearer " + jwt
-            }
-        });
+const MyPage = () => {
+    
+    useEffect(()=>{
+        getLoginData();
     },[]);
+    
 
     const {logOut} = useAuth();
     return (
