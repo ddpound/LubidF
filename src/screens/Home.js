e7752363dd136ca67d87c263b28623d9d1fb0935
dpound@naver.com <dpound@naver.com>
2024-08-21 21:07:57 +0900

    하단 plus 버튼 파일 위치 수정 및 기존의 메인 view 에서 추가로 덧씌우는 View를 하나 추가하여 relative 포지션을 넣어 하단 고정된것처럼 보이게 수정완료

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,87 +1,153 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context";
-import { JwtSotrageCheck, JwtStorageSet, JwtStorageData } from "../components/AsyncData/JwtAsyncData";
-import { useState } from "react";
-import HomeSection from "./Home/HomeSection";
-import ProductSection from "./Home/ProductSection";
-import AuctionScheduleSection from "./Home/AuctionScheduleSection";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {
+  JwtSotrageCheck,
+  JwtStorageSet,
+  JwtStorageData,
+} from '../components/AsyncData/JwtAsyncData';
+import {useState} from 'react';
+import HomeSection from './Home/HomeSection';
+import ProductSection from './Home/ProductSection';
+import AuctionScheduleSection from './Home/AuctionScheduleSection';
 
-const checkJwt = async () =>{
-    let jwtValue = await JwtStorageData("LubidJwt");
-    console.log('homePage' , jwtValue);
-    return jwtValue;
-}
+const checkJwt = async () => {
+  let jwtValue = await JwtStorageData('LubidJwt');
+  console.log('homePage', jwtValue);
+  return jwtValue;
+};
 
-const Home = ()=>{
-    const [pageState, setPageState] = useState(1);
-    
-    return (
-        <>
-            <ScrollView>
-                <SafeAreaView>
-                    <View style={styles.mainPageHeader}>
-                        <Text style={styles.headerText}>L U B I D</Text>
-                    </View>
-                    <View style={styles.menuBarContainer}>
-                        <TouchableOpacity style={pageState == 1 ? styles.topMenuButtonPoint : styles.topMenuButton } onPress={()=>{setPageState(1)}}>
-                            <Text style={styles.topMenuButtonText}>홈</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={pageState == 2 ? styles.topMenuButtonPoint : styles.topMenuButton } onPress={()=>{setPageState(2)}} >
-                            <Text style={styles.topMenuButtonText}>상품</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={pageState == 3 ? styles.topMenuButtonPoint : styles.topMenuButton } onPress={()=>{setPageState(3)}}>
-                            <Text style={styles.topMenuButtonText}>경매일정</Text>
-                        </TouchableOpacity>
-                    </View>
-                    {
-                        pageState == 1 && <HomeSection/>
-                        
-                    }
-                    {
-                        pageState == 2 && <ProductSection/>
-                    }
-                    {
-                        pageState == 3 && <AuctionScheduleSection/>   
-                    }
-                </SafeAreaView>
-            </ScrollView>
-        </>
-    )
-}
+const Home = () => {
+  const [pageState, setPageState] = useState(1);
+
+  return (
+    <>
+      <View style={styles.releativeContainer}>
+        <ScrollView style={styles.homeMainContainer}>
+          <SafeAreaView>
+            <View style={styles.mainPageHeader}>
+              <Text style={styles.headerText}>L U B I D</Text>
+            </View>
+            <View style={styles.menuBarContainer}>
+              <TouchableOpacity
+                style={
+                  pageState == 1
+                    ? styles.topMenuButtonPoint
+                    : styles.topMenuButton
+                }
+                onPress={() => {
+                  setPageState(1);
+                }}>
+                <Text style={styles.topMenuButtonText}>홈</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={
+                  pageState == 2
+                    ? styles.topMenuButtonPoint
+                    : styles.topMenuButton
+                }
+                onPress={() => {
+                  setPageState(2);
+                }}>
+                <Text style={styles.topMenuButtonText}>상품</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={
+                  pageState == 3
+                    ? styles.topMenuButtonPoint
+                    : styles.topMenuButton
+                }
+                onPress={() => {
+                  setPageState(3);
+                }}>
+                <Text style={styles.topMenuButtonText}>경매일정</Text>
+              </TouchableOpacity>
+            </View>
+            {pageState == 1 && <HomeSection />}
+            {pageState == 2 && <ProductSection />}
+            {pageState == 3 && <AuctionScheduleSection />}
+          </SafeAreaView>
+        </ScrollView>
+        <View style={styles.fixedBox}>
+          <TouchableOpacity style={styles.absolutePlusMenuButton}>
+            <Text>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
 
 export default Home;
 
 const styles = StyleSheet.create({
-    mainPageHeader : {
-        borderBottomColor : "black",
-    },
-    headerText :{
-        color : 'black',
-        fontSize : 20,
-        margin: 15,
-    },
-    menuBarContainer : {
-        color : 'black',
-        flexDirection : 'row',
-    },
-    bodyView : {
-        color : 'black',
-    },
-    menuText : {
-        color : 'black',
-        margin: 10,
-        fontSize: 18,
-    },
-    topMenuButton: {
-        width : '25%',
-    },
-    topMenuButtonPoint: {
-        width : '25%',
-        backgroundColor: 'gray',
-    },
-    topMenuButtonText : {
-        color : 'black',
-        fontSize : 20,
-        textAlign: 'center',
-    },
+  releativeContainer: {
+    maxHeight: '100%',
+    position: 'relative',
+  },
+  mainPageHeader: {
+    borderBottomColor: 'black',
+  },
+  headerText: {
+    color: 'black',
+    fontSize: 20,
+    margin: 15,
+  },
+  menuBarContainer: {
+    color: 'black',
+    flexDirection: 'row',
+  },
+  bodyView: {
+    color: 'black',
+  },
+  menuText: {
+    color: 'black',
+    margin: 10,
+    fontSize: 18,
+  },
+  topMenuButton: {
+    width: '25%',
+  },
+  topMenuButtonPoint: {
+    width: '25%',
+    backgroundColor: 'gray',
+  },
+  topMenuButtonText: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  fixedBox: {
+    position: 'absolute', // absolute 위치 지정
+    bottom: 10, // 화면의 하단에서 10px
+    right: 10, // 화면의 오른쪽에서 10px
+    backgroundColor: 'yellow',
+    padding: 10,
+    borderRadius: 5,
+    zIndex: 999,
+  },
+  absolutePlusMenuButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'pink',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+  },
 });
--- src/screens/Home/HomeSection.js
+++ src/screens/Home/HomeSection.js
@@ -12,81 +12,47 @@ import CommonViewBox from '../../components/Common/CommonViewBox';
 import {useEffect, useRef} from 'react';
 
 const HomeSection = () => {
-  const scrollY = useRef(new Animated.Value(0)).current;
-
-  const handleScroll = event => {
-    console.log('test', event);
-  };
-
-  const buttonTranslateY = scrollY.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, -1], // 스크롤에 따라 위치 조정
-    extrapolate: 'clamp',
-  });
-
-  useEffect(() => {
-    console.log(buttonTranslateY);
-  }, [buttonTranslateY]);
-
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView
-        contentContainerStyle={styles.scrollViewContent}
-        onScroll={handleScroll}
-        scrollEventThrottle={0.5}>
-        <View style={styles.bannerContainer}>
-          <Text style={styles.sectionTitleText}>Banner</Text>
-        </View>
-        <View>
-          <HomeSectionCircle />
-        </View>
-        <View style={styles.sectionTitleView}>
-          <Text style={styles.sectionTitleText}>진행중인 라이브</Text>
-        </View>
-        <View style={styles.viewBoxList}>
-          <CommonViewBox />
-          <CommonViewBox />
-        </View>
-        <View style={styles.sectionTitleView}>
-          <Text style={styles.sectionTitleText}>BEST</Text>
-        </View>
-        <View style={styles.viewBoxList}>
-          <CommonViewBox />
-          <CommonViewBox />
-        </View>
-        <View style={styles.sectionTitleView}>
-          <Text style={styles.sectionTitleText}>종료 임박한 상품</Text>
-        </View>
-        <View style={styles.viewBoxList}>
-          <CommonViewBox />
-          <CommonViewBox />
-        </View>
-        <View style={styles.sectionTitleView}>
-          <Text style={styles.sectionTitleText}>예정된 경매</Text>
-        </View>
-        <View style={styles.viewBoxList}>
-          <CommonViewBox />
-          <CommonViewBox />
-        </View>
-        <View style={styles.sectionTitleView}>
-          <TouchableOpacity style={styles.wishButton}>
-            <Text style={styles.wishButtonText}>위시가 없나요?</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.fixedBox}>
-          <TouchableOpacity style={styles.absolutePlusMenuButton}>
-            <Text style={styles.wishButtonText}>+</Text>
-          </TouchableOpacity>
-        </View>
-        <Animated.View
-          style={[
-            styles.button,
-            {transform: [{translateY: buttonTranslateY}]},
-          ]}>
-          <Text style={styles.buttonText}>Scroll Following Button</Text>
-        </Animated.View>
-      </ScrollView>
+    <SafeAreaView>
+      <View style={styles.bannerContainer}>
+        <Text style={styles.sectionTitleText}>Banner</Text>
+      </View>
+      <View>
+        <HomeSectionCircle />
+      </View>
+      <View style={styles.sectionTitleView}>
+        <Text style={styles.sectionTitleText}>진행중인 라이브</Text>
+      </View>
+      <View style={styles.viewBoxList}>
+        <CommonViewBox />
+        <CommonViewBox />
+      </View>
+      <View style={styles.sectionTitleView}>
+        <Text style={styles.sectionTitleText}>BEST</Text>
+      </View>
+      <View style={styles.viewBoxList}>
+        <CommonViewBox />
+        <CommonViewBox />
+      </View>
+      <View style={styles.sectionTitleView}>
+        <Text style={styles.sectionTitleText}>종료 임박한 상품</Text>
+      </View>
+      <View style={styles.viewBoxList}>
+        <CommonViewBox />
+        <CommonViewBox />
+      </View>
+      <View style={styles.sectionTitleView}>
+        <Text style={styles.sectionTitleText}>예정된 경매</Text>
+      </View>
+      <View style={styles.viewBoxList}>
+        <CommonViewBox />
+        <CommonViewBox />
+      </View>
+      <View style={styles.sectionTitleView}>
+        <TouchableOpacity style={styles.wishButton}>
+          <Text style={styles.wishButtonText}>위시가 없나요?</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -94,14 +60,6 @@ const HomeSection = () => {
 export default HomeSection;
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-  scrollViewContent: {
-    flex: 1,
-    flexGrow: 1,
-    paddingBottom: 100, // 버튼과 콘텐츠가 겹치지 않도록 여유 공간 추가
-  },
   sectionTitleView: {
     marginTop: 10,
     marginBottom: 5,
@@ -133,33 +91,5 @@ const styles = StyleSheet.create({
     fontSize: 23,
     fontWeight: '400',
     marginTop: 10,
-  },
-  fixedBox: {
-    position: 'absolute', // absolute 위치 지정
-    bottom: 10, // 화면의 하단에서 10px
-    right: 10, // 화면의 오른쪽에서 10px
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-    zIndex: 999,
-  },
-  absolutePlusMenuButton: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
   },
 });
